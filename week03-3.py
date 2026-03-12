bad3d7f95595bceade4b7f2254233ff411c8a2ca
clawdeenhsia <91250836+clawdeenhsia@users.noreply.github.com>
2026-03-12 16:23:30 +0800

    week03

---
+++ week03-3.py
@@ -0,0 +1,15 @@
+#week03-3.py
+#leetcode 1456
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels=set('aeiou')
+        count=0
+        for i in range(k):
+            if s[i] in vowels:count+=1
+        ans=count
+        N=len(s)
+        for i in range(k,N):
+            if s[i] in vowels:count+=1
+            if s[i-k] in vowels:count-=1
+            ans=max(ans,count)
+        return ans
