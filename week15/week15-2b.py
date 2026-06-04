4ecc6ccc7de343c63b4c7b84a32ff9428bf15692
clawdeenhsia <91250836+clawdeenhsia@users.noreply.github.com>
2026-06-04 16:24:25 +0800

    week15

---
+++ week15/week15-2b.py
@@ -0,0 +1,11 @@
+#week15-2b.py
+#leetcode 1143
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        M,N=len(text1),len(text2)
+        table=[ [0] *(N+1) for i in  range(M+1)]
+        for i in range(M):
+             for j in range(N):
+                if text1[i]==text2[j]:table[i+1][j+1]=table[i][j]+1
+                table[i+1][j+1]=max(table[i+1][j+1],table[i][j+1],table[i+1][j])
+        return table[M][N]
---
+++ week15/week15-3.py
@@ -0,0 +1,12 @@
+#week15-3.py
+#leetcode 714
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        @cache
+        def helper(i,hasStack):
+            if i==len(prices):return 0
+            if hasStack: ans=prices[i]+helper(i+1,False)-fee
+            else: ans=-prices[i]+helper(i+1,True)
+            helper(i+1,hasStack)
+            return max(ans,helper(i+1,hasStack))
+        return helper(0,False)
